e0dc0a686d107be77e0b5fb764345e4908bbc93a
Proenca13 <henriqueproenca2004@hotmai.com>
2024-11-04 11:00:02 +0000

    db almost loads

--- sql/schema.sql
+++ sql/schema.sql
@@ -10,13 +10,13 @@ DROP TABLE IF EXISTS Report CASCADE;
 DROP TABLE IF EXISTS Review CASCADE;
 DROP TABLE IF EXISTS Order_Product CASCADE;
 DROP TABLE IF EXISTS Transaction CASCADE;
-DROP TABLE IF EXISTS Order CASCADE;
+DROP TABLE IF EXISTS "Order" CASCADE;
 DROP TABLE IF EXISTS Shopping_Cart CASCADE;
 DROP TABLE IF EXISTS Wishlist CASCADE;
 DROP TABLE IF EXISTS Admin;
 DROP TABLE IF EXISTS Product CASCADE;
 DROP TABLE IF EXISTS Category CASCADE;
-DROP TABLE IF EXISTS User CASCADE;
+DROP TABLE IF EXISTS "User" CASCADE;
 
 -----------------------------------------
 -- Drop Types
@@ -60,7 +60,7 @@ CREATE TYPE order_status AS ENUM(
 -- Tables
 -----------------------------------------
 
-CREATE TABLE User (
+CREATE TABLE "User" (
     id INT PRIMARY KEY,
     username TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL,
@@ -79,87 +79,87 @@ CREATE TABLE Category (
 
 CREATE TABLE Product (
     id INT PRIMARY KEY,
-    category_id INT NOT NULL UNIQUE ON UPDATE CASCADE,
+    category_id INT NOT NULL UNIQUE ,
     name TEXT NOT NULL,
     image TEXT,
     description TEXT,
     quantity INT CHECK (quantity >= 0),
     price NUMERIC(10, 2) NOT NULL CHECK (price >= 0),
     discount_percent NUMERIC(5, 2) CHECK (discount_percent >= 0 AND discount_percent <= 100),
-    FOREIGN KEY (category_id) REFERENCES Category(id)
+    FOREIGN KEY (category_id) REFERENCES Category(id) ON UPDATE CASCADE
 );
 
 CREATE TABLE Wishlist (
     id INT PRIMARY KEY,
-    user_id INT NOT NULL ON UPDATE CASCADE,
-    product_id INT NOT NULL ON UPDATE CASCADE,
+    user_id INT NOT NULL ,
+    product_id INT NOT NULL ,
     UNIQUE (user_id, product_id),
-    FOREIGN KEY (user_id) REFERENCES User(id),
-    FOREIGN KEY (product_id) REFERENCES Product(id)
+    FOREIGN KEY (user_id) REFERENCES "User"(id) ON UPDATE CASCADE,
+    FOREIGN KEY (product_id) REFERENCES Product(id) ON UPDATE CASCADE
 );
 
 CREATE TABLE Shopping_Cart (
     id INT PRIMARY KEY,
-    user_id INT NOT NULL ON UPDATE CASCADE,
-    product_id INT NOT NULL ON UPDATE CASCADE,
+    user_id INT NOT NULL ,
+    product_id INT NOT NULL ,
     UNIQUE (user_id, product_id),
-    FOREIGN KEY (user_id) REFERENCES User(id),
-    FOREIGN KEY (product_id) REFERENCES Product(id)
+    FOREIGN KEY (user_id) REFERENCES "User"(id) ON UPDATE CASCADE,
+    FOREIGN KEY (product_id) REFERENCES Product(id) ON UPDATE CASCADE
 );
 
-CREATE TABLE Order (
+CREATE TABLE "Order" (
     id INT PRIMARY KEY,
-    user_id INT NOT NULL UNIQUE ON UPDATE CASCADE,
+    user_id INT NOT NULL UNIQUE ,
     tracking_number TEXT UNIQUE NOT NULL,
     status order_status NOT NULL,
     buy_date DATE NOT NULL,
     estimated_delivery_date DATE NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES User(id)
+    FOREIGN KEY (user_id) REFERENCES "User"(id) ON UPDATE CASCADE
 );
 
 CREATE TABLE Transaction (
     id INT PRIMARY KEY,
-    user_id INT NOT NULL UNIQUE ON UPDATE CASCADE,
-    order_id INT NOT NULL UNIQUE ON UPDATE CASCADE,
+    user_id INT NOT NULL UNIQUE ,
+    order_id INT NOT NULL UNIQUE ,
     code TEXT UNIQUE NOT NULL,
     price NUMERIC(10, 2) NOT NULL CHECK (price >= 0),
     NIF TEXT NOT NULL,
     credit_card_number TEXT NOT NULL,
     credit_card_exp_date DATE NOT NULL,
     credit_card_cvv TEXT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES User(id),
-    FOREIGN KEY (order_id) REFERENCES Order(id)
+    FOREIGN KEY (user_id) REFERENCES "User"(id) ON UPDATE CASCADE,
+    FOREIGN KEY (order_id) REFERENCES "Order"(id) ON UPDATE CASCADE
 );
 
 CREATE TABLE Order_Product (
     id INT PRIMARY KEY,
-    order_id INT NOT NULL ON UPDATE CASCADE,
-    product_id INT NOT NULL ON UPDATE CASCADE,
+    order_id INT NOT NULL ,
+    product_id INT NOT NULL ,
     UNIQUE (order_id, product_id),
-    FOREIGN KEY (order_id) REFERENCES Order(id),
-    FOREIGN KEY (product_id) REFERENCES Product(id)
+    FOREIGN KEY (order_id) REFERENCES "Order"(id) ON UPDATE CASCADE,
+    FOREIGN KEY (product_id) REFERENCES Product(id) ON UPDATE CASCADE
 );
 
 CREATE TABLE Review (
     id INT PRIMARY KEY,
-    user_id INT NOT NULL ON UPDATE CASCADE,
-    product_id INT NOT NULL ON UPDATE CASCADE,
+    user_id INT NOT NULL ,
+    product_id INT NOT NULL ,
     description TEXT NOT NULL,
     rating NUMERIC(3, 1) CHECK (rating >= 0 AND rating <= 5),
-    FOREIGN KEY (user_id) REFERENCES User(id),
-    FOREIGN KEY (product_id) REFERENCES Product(id)
+    FOREIGN KEY (user_id) REFERENCES "User"(id) ON UPDATE CASCADE,
+    FOREIGN KEY (product_id) REFERENCES Product(id) ON UPDATE CASCADE
 );
 
 CREATE TABLE Report (
     id INT PRIMARY KEY,
-    review_id INT NOT NULL ON UPDATE CASCADE,
-    user_id INT NOT NULL ON UPDATE CASCADE,
-    admin_id INT NOT NULL ON UPDATE CASCADE,
+    review_id INT NOT NULL ,
+    user_id INT NOT NULL ,
+    admin_id INT NOT NULL,
     reason TEXT NOT NULL,
     description TEXT,
-    FOREIGN KEY (review_id) REFERENCES Review(id),
-    FOREIGN KEY (user_id) REFERENCES User(id),
-    FOREIGN KEY (admin_id) REFERENCES Admin(id)
+    FOREIGN KEY (review_id) REFERENCES Review(id) ON UPDATE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES "User"(id) ON UPDATE CASCADE,
+    FOREIGN KEY (admin_id) REFERENCES Admin(id) ON UPDATE CASCADE
 );
 
 CREATE TABLE Notification (
@@ -171,11 +171,11 @@ CREATE TABLE Notification (
 
 CREATE TABLE Notification_User (
     id INT PRIMARY KEY,
-    user_id INT NOT NULL ON UPDATE CASCADE,
-    notification_id INT NOT NULL ON UPDATE CASCADE,
+    user_id INT NOT NULL ,
+    notification_id INT NOT NULL,
     UNIQUE (user_id, notification_id),
-    FOREIGN KEY (user_id) REFERENCES User(id),
-    FOREIGN KEY (notification_id) REFERENCES Notification(id)
+    FOREIGN KEY (user_id) REFERENCES "User"(id) ON UPDATE CASCADE,
+    FOREIGN KEY (notification_id) REFERENCES Notification(id) ON UPDATE CASCADE
 );
 
 CREATE TABLE Shipping_Address (
@@ -189,30 +189,30 @@ CREATE TABLE Shipping_Address (
 
 CREATE TABLE User_Shipping (
     id INT PRIMARY KEY,
-    user_id INT NOT NULL ON UPDATE CASCADE,
-    shipping_id INT NOT NULL ON UPDATE CASCADE,
+    user_id INT NOT NULL ,
+    shipping_id INT NOT NULL ,
     UNIQUE (user_id, shipping_id),
-    FOREIGN KEY (user_id) REFERENCES User(id),
-    FOREIGN KEY (shipping_id) REFERENCES Shipping_Address(id)
+    FOREIGN KEY (user_id) REFERENCES "User"(id) ON UPDATE CASCADE,
+    FOREIGN KEY (shipping_id) REFERENCES Shipping_Address(id) ON UPDATE CASCADE
 );
 
 
 -----------------------------------------
 -- PERFORMANCE INDICES
 -----------------------------------------
-CREATE INDEX idx_order_dates ON order USING btree (buy_date, estimated_delivery_date); 
-CLUSTER order USING idx_order_dates;
+CREATE INDEX idx_order_dates ON "Order" USING btree (buy_date, estimated_delivery_date); 
+CLUSTER "Order" USING idx_order_dates;
 
 CREATE INDEX idx_notification_date ON notification USING btree (date); 
 CLUSTER notification USING idx_notification_date;
 
-CREATE INDEX idx_product_price ON product (price);
-CLUSTER product USING idx_product_price;
+CREATE INDEX idx_product_price ON Product (price);
+CLUSTER Product USING idx_product_price;
 
 CREATE INDEX idx_transaction_price ON transaction (price);
 CLUSTER transaction USING idx_transaction_price;
 
-CREATE INDEX idx_review_rating ON review (rating);
+CREATE INDEX idx_review_rating ON Review (rating);
 
 -----------------------------------------
 -- FTS INDICES
@@ -260,9 +260,9 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-DROP TRIGGER IF EXISTS trg_order_cancellation ON Order;
+DROP TRIGGER IF EXISTS trg_order_cancellation ON "Order";
 CREATE TRIGGER trg_order_cancellation
-BEFORE UPDATE ON Order
+BEFORE UPDATE ON "Order"
 FOR EACH ROW EXECUTE FUNCTION enforce_order_cancellation();
 
 -- trigger02
@@ -274,9 +274,9 @@ BEGIN
     RETURN NEW;
 END
 $$ LANGUAGE plpgsql;
-DROP TRIGGER IF EXISTS trg_admin_purchase_prevention ON ShoppingCart;
+DROP TRIGGER IF EXISTS trg_admin_purchase_prevention ON Shopping_Cart;
 CREATE TRIGGER trg_admin_purchase_prevention
-BEFORE INSERT ON ShoppingCart
+BEFORE INSERT ON Shopping_Cart
 FOR EACH ROW EXECUTE PROCEDURE prevent_admin_purchase();                           
 
 
@@ -284,7 +284,7 @@ FOR EACH ROW EXECUTE PROCEDURE prevent_admin_purchase();
 CREATE OR REPLACE FUNCTION verify_purchase_for_review() RETURNS TRIGGER AS $$
 BEGIN
     IF (SELECT COUNT(*) FROM Order_Product op
-        JOIN Order o ON op.order_id = o.id
+        JOIN "Order" o ON op.order_id = o.id
         WHERE op.product_id = NEW.product_id AND o.user_id = NEW.user_id) = 0 THEN
         RAISE EXCEPTION 'Only users who have purchased this product can leave a review';
     END IF;
@@ -322,9 +322,9 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-DROP TRIGGER IF EXISTS trg_restrict_out_of_stock_cart_addition ON ShoppingCart;
+DROP TRIGGER IF EXISTS trg_restrict_out_of_stock_cart_addition ON Shopping_Cart;
 CREATE TRIGGER trg_restrict_out_of_stock_cart_addition
-BEFORE INSERT ON ShoppingCart
+BEFORE INSERT ON Shopping_Cart
 FOR EACH ROW EXECUTE FUNCTION restrict_out_of_stock_cart_addition();
 
 -- trigger06
@@ -368,9 +368,9 @@ BEGIN
 END
 $$ LANGUAGE plpgsql;
 
-DROP TRIGGER IF EXISTS trg_own_shopping_cart ON ShoppingCart;
+DROP TRIGGER IF EXISTS trg_own_shopping_cart ON Shopping_Cart;
 CREATE TRIGGER trg_own_shopping_cart
-BEFORE INSERT ON ShoppingCart
+BEFORE INSERT ON Shopping_Cart
 FOR EACH ROW EXECUTE PROCEDURE enforce_own_shopping_cart();
 
 -- trigger09
@@ -422,7 +422,7 @@ FOR EACH ROW EXECUTE PROCEDURE restrict_out_of_stock_wishlist_addition();
 -- trigger 12
 CREATE OR REPLACE FUNCTION restrict_address_change_for_shipped_orders() RETURNS TRIGGER AS $$
 BEGIN
-    IF (SELECT status FROM Order WHERE shipping_address_id = OLD.id) = 'shipped' THEN
+    IF (SELECT status FROM "Order" WHERE shipping_address_id = OLD.id) = 'shipped' THEN
         RAISE EXCEPTION 'Cannot change the shipping address for shipped orders';
     END IF;
     RETURN NEW;
