a6a124c7608c54d5a71e5b41d7f2265e10193f9b
Eduardo <edumrcunha@gmail.com>
2024-12-09 19:24:07 +0000

    contextual help edit profile

--- resources/views/Editprofile.blade.php
+++ resources/views/Editprofile.blade.php
@@ -32,7 +32,10 @@
                 
                 <div class="form-group">
                     <label for="email">Email</label>
-                    <input type="email" id="email" name="email" class="form-control" value="{{ old('email', Auth::user()->email) }}" required>
+                    <div class="input-container">
+                        <input type="email" id="email" name="email" class="form-control" value="{{ old('email', Auth::user()->email) }}" required>
+                        <span class="tooltip-icon" data-tooltip="Enter a valid email address (e.g., example@example.com)">?</span>
+                    </div>
                     @error('email')
                         <div class="text-danger">{{ $message }}</div>
                     @enderror
@@ -41,7 +44,10 @@
                 
                 <div class="form-group">
                     <label for="password">New Password (leave blank to keep current password)</label>
-                    <input type="password" id="password" name="password" class="form-control">
+                    <div class="input-container">
+                        <input type="password" id="password" name="password" class="form-control">
+                        <span class="tooltip-icon" data-tooltip="Password must be at least 8 characters long and include an uppercase letter, a lowercase letter, and a number.">?</span>
+                    </div>
                     @error('password')
                         <div class="text-danger">{{ $message }}</div>
                     @enderror
@@ -50,7 +56,10 @@
                 
                 <div class="form-group">
                     <label for="password_confirmation">Confirm New Password</label>
-                    <input type="password" id="password_confirmation" name="password_confirmation" class="form-control">
+                    <div class="input-container">
+                        <input type="password" id="password_confirmation" name="password_confirmation" class="form-control">
+                        <span class="tooltip-icon" data-tooltip="Ensure your password confirmation matches the password.">?</span>
+                    </div>
                     @error('password_confirmation')
                         <div class="text-danger">{{ $message }}</div>
                     @enderror
